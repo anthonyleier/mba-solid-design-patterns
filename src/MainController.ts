d88e249339759cd6fd292ed032af1df9108c056a
Anthony Cruz <anthonyleierlw@gmail.com>
2023-11-01 10:01:14 -0300

    Controllers e Adpters para Express

---
+++ src/MainController.ts
@@ -0,0 +1,15 @@
+import HttpServer from "./HttpServer";
+import Usecase from "./Usecase";
+
+export default class MainController {
+    constructor(readonly httpServer: HttpServer, readonly usecase: Usecase) {
+        httpServer.on("post", "/generate_invoices", async function (params: any, body: any, headers: any) {
+            const input = body;
+            input.userAgent = headers["user-agent"];
+            input.host = headers["host"];
+
+            const output = await usecase.execute(input);
+            return output;
+        });
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import ContractDatabaseRepository from "./ContractDatabaseRepository";
+import ExpressAdapter from "./ExpressAdapter";
+import GenerateInvoices from "./GenerateInvoices";
+import LoggerDecorator from "./LoggerDecorator";
+import MainController from "./MainController";
+import PgPromiseAdapter from "./PgPromiseAdapter";
+
+const connection = new PgPromiseAdapter();
+const contractRepository = new ContractDatabaseRepository(connection);
+const generateInvoices = new LoggerDecorator(new GenerateInvoices(contractRepository));
+const httpServer = new ExpressAdapter();
+new MainController(httpServer, generateInvoices);
+httpServer.listen(3000);
